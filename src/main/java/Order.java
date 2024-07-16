c33267ca81a0381130c782d0dd2fcaa25d20fb13
ursulakohler <kohlerursula22@gmail.com>
2024-07-16 16:13:26 +0200

    add OrderStatus2

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -9,11 +9,7 @@ import java.util.List;
 
 public record Order(
         String id,
-        List<Product> products)
+        List<Product> products,
+        OrderStatus orderStatus)
 {
-    public enum OrderStatus {
-        PROCESSING,
-        IN_DELIVERY,
-        COMPLETED
-    }
 }
